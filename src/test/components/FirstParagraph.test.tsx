a8942bdfdc6daf00bb98950f0c531baac7e75752
Teh|Striker <kristianhanus@gmail.com>
2024-03-03 13:14:21 +0100

    Add first component test

--- src/test/components/FirstParagraph.test.tsx
+++ src/test/components/FirstParagraph.test.tsx
@@ -1,6 +1,9 @@
+import React from "react";
 import '@testing-library/jest-dom'
 import { render } from "@testing-library/react"
+import FirstParagraph from "../../components/FirstParagraph"
 
-test('demo', () => {
-    expect(true).toBe(true)
+test("Render paragraph component", () => {
+    const tree = render(<FirstParagraph />)
+    expect(tree).toMatchSnapshot()
 })
